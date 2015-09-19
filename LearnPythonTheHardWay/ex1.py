668a74c12dfa769cf998a93cd0f824c3416d18ce
Kamil Sokolowski <kamil.sokolowski@example.com>
2015-09-19 22:58:42 +0200

    [LPTHW] finished Exercise 1

---
+++ LearnPythonTheHardWay/ex1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2
+
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather You 'not'."
+print 'I "said" do not touch this.'
+print 'This is the "another" line.'
+
