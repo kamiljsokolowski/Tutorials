47bfe6bc60e60224e259ef168ac4a6d83a8cb7fe
Kamil Sokolowski <kamil.j.sokolowski@gmail.com>
2015-09-29 12:44:39 +0200

    [ThinkPython] ex2 problem solution

---
+++ ThinkPython/chap2/ex2.py
@@ -0,0 +1,24 @@
+width = 17
+height = 12.0
+delimiter = '.'
+
+print "value == 8, type int"
+print width/2
+print type(width/2)
+
+print "value == 8.5, type float"
+print width/2.0
+print type(width/2.0)
+
+print "value == 4.0, type float"
+print height/3
+print type(height/3)
+
+print "value == 11, type int"
+print 1 + 2 * 5
+print type(1 + 2 * 5)
+
+print 'value == ".....", type str'
+print delimiter * 5
+print type(delimiter * 5)
+
