54457232bf884f1fb5b9a530c55f4ce8c8ac4739
Kamil Sokolowski <kamil.j.sokolowski@gmail.com>
2015-10-13 22:59:02 +0200

    [ThinkPython] chap6/ex1 problem solution

---
+++ ThinkPython/chap6/ex1.py
@@ -0,0 +1,12 @@
+def compare(x, y):
+    if x > y:
+        return 1
+    elif x == y:
+        return 0
+    elif x < y:
+        return -1
+
+print compare(3,2)
+print compare(2,2)
+print compare(1,2)
+
