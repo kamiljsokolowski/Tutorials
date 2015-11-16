96e603e6d01ea7e7d33416d3d7d4fc07c93205e3
Kamil Sokolowski <kamil.j.sokolowski@gmail.com>
2015-11-16 21:35:20 +0100

    remove dotfiles setup from env boostrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-# dotfiles
-mv .vimrc .vimrc.terry
-mkdir .dotfiles
-cd .dotfiles
-git clone https://github.com/sokolowskik/dotfiles.git .
-chmod +x bootstrap.sh
-./bootstrap.sh
-
 ### development tools
 # the essential stuff
 sudo pacman -Sy --noconfirm base-devel
