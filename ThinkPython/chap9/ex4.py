6b6c8ab1b2c1a9031611de7822e6226630cbf2e0
Kamil Sokolowski <kamil.j.sokolowski@gmail.com>
2015-11-21 10:18:44 +0100

    [ThinkPython] chap9/ex4 problem solution

---
+++ ThinkPython/chap9/ex4.py
@@ -0,0 +1,22 @@
+file = raw_input('filename: ')
+string = raw_input('Insert a string of forbidden characters: ')
+
+def uses_only(w, s):
+    """
+    Check if the word consists only of the characters specified in a string.
+    Return False if it does not
+    """
+    for l in w:
+        if s.find(l) == -1:
+            return False
+    return True
+
+f = open(file)
+
+for word in f:
+    word = word.strip()
+    if uses_only(word, string):
+        print 'Word', word, 'consists only of characters specified in', string
+
+f.close()
+
