c553c440eceb8282bb0ab805f4b2434e629bea62
Kamil Sokolowski <kamil.j.sokolowski@gmail.com>
2015-11-26 15:48:07 +0100

    migrate env OS platform to Ubuntu

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
+# db sync first
+sudo apt-get update
+
 ### virtualization
 # Docker
-sudo pacman -Sy --noconfirm docker
-sudo systemctl enable docker
-sudo systemctl start docker
+sudo apt-get install -y linux-image-extra-$(uname -r)
+sudo echo aufs >> /etc/modules
+sudo modprobe aufs
+curl -sSL https://get.docker.com | sh
 
 ### development tools
 # the essential stuff
-sudo pacman -Sy --noconfirm base-devel
+sudo apt-get install -y build-essential
 
 # Python
-sudo pacman -Sy --noconfirm python2-pip python2-virtualenv
-#sudo pacman -Sy --noconfirm python-pip python-virtualenv
+sudo apt-get install -y python-setuptools
+sudo easy_install pip
+sudo pip install virtualenv
