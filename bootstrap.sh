16e679fac1bac603b810bc00dd0bda05573a9e58
Kamil Sokolowski <kamil.j.sokolowski@gmail.com>
2015-11-28 09:21:23 +0100

    sync local DB before bootstrapping

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 
-# db sync first
-sudo apt-get update
+sudo apt-get update -q && sudo apt-get install -y \
+    linux-image-extra-$(uname -r) \
+    build-essential \
+    python-setuptools
 
 ### virtualization
 # Docker
-sudo apt-get install -y linux-image-extra-$(uname -r)
+bash <<EODOCKER
 sudo echo aufs >> /etc/modules
 sudo modprobe aufs
 curl -sSL https://get.docker.com | sh
+EODOCKER
 
-### development tools
-# the essential stuff
-sudo apt-get install -y build-essential
-
+### develop
 # Python
-sudo apt-get install -y python-setuptools
+bash <<EOPYTHON
 sudo easy_install pip
 sudo pip install virtualenv
+EOPYTHON
+
