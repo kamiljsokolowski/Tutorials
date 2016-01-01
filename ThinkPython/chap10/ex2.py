dcc419a047a5978ede88817a4d7b56c3ad1d8629
Kamil Sokolowski <kamil.j.sokolowski@gmail.com>
2016-01-01 23:26:46 +0100

    [ThinkPython] chap10/ex2 problem solution

---
+++ ThinkPython/chap10/ex2.py
@@ -0,0 +1,29 @@
+test1 = [['John','merry','alan'],['dwight','Bill'],['monica','sarah','Jerry','Samuel']]
+test2 = [['John','merry','alan'],['d','Bill'],['monica','sarah','J','Samuel']]
+test3 = [['John','merry','alan'],['dwight','2'],['monica','sarah','Jerry','Samuel']]
+test4 = [['John','merry','alan'],['dwight',2],['monica','sarah','Jerry','Samuel']]
+
+def capitalize_all(l):
+    res = []
+    for s in l:
+        if type(s) != str:
+                return False
+        res.append(s.capitalize())
+    return res
+
+def capitalize_nested(n):
+    """
+    Capitalizie all elements from all nested lists
+    """
+    res = []
+    for l in n:
+        res.append(capitalize_all(l))
+        if not capitalize_all(l):
+            return False
+    return res
+
+print capitalize_nested(test1)
+print capitalize_nested(test2)
+print capitalize_nested(test3)
+print capitalize_nested(test4)
+
