1fa0ffdb98c516dabb8db56bd2f9ef8b07085dcf
Kamil Sokolowski <kamil.j.sokolowski@gmail.com>
2016-02-19 13:34:20 +0100

    [ThinkPython] substitute for loops with list comprehensions in chap10/ex2

--- ThinkPython/chap10/ex2.py
+++ ThinkPython/chap10/ex2.py
@@ -4,22 +4,24 @@ test3 = [['John','merry','alan'],['dwight','2'],['monica','sarah','Jerry','Samue
 test4 = [['John','merry','alan'],['dwight',2],['monica','sarah','Jerry','Samuel']]
 
 def capitalize_all(l):
-    res = []
-    for s in l:
-        if type(s) != str:
-                return False
-        res.append(s.capitalize())
+#    res = []
+#    for s in l:
+#        if type(s) != str:
+#                return False
+#        res.append(s.capitalize())
+    res = [s.capitalize() for s in l if type(s) == str]    
     return res
 
 def capitalize_nested(n):
     """
     Capitalizie all elements from all nested lists
     """
-    res = []
-    for l in n:
-        res.append(capitalize_all(l))
-        if not capitalize_all(l):
-            return False
+#    res = []
+#    for l in n:
+#        res.append(capitalize_all(l))
+#        if not capitalize_all(l):
+#            return False
+    res = [capitalize_all(l) for l in n if capitalize_all(l)]
     return res
 
 print capitalize_nested(test1)
