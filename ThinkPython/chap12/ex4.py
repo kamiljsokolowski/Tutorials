a34e3fc40f996be75db65e77b5ded5c4112f849c
Kamil Sokolowski <kamil.j.sokolowski@gmail.com>
2016-03-07 12:00:07 +0100

    [ThinkPython] chap12/ex4 problem part 1 solution

---
+++ ThinkPython/chap12/ex4.py
@@ -0,0 +1,51 @@
+f = raw_input('filename [words.txt]:') or 'words.txt'
+
+#def is_anagram(ref, word):
+#    """
+#    1. Take two words one being the reference.
+#    2. Return True if the two are anagrams.
+#    """
+#    ref_to_list = list(ref)
+#    word_to_list = list(word)
+#    ref_sort = ref_to_list.sort()
+#    word_sort = word_to_list.sort()
+#    if ref_sort is word_sort:
+#        return True
+
+def list_words(f):
+    """
+    1. Take a text file and extract all the words separated by space and new line characters.
+    2. Remove special characters.
+    3. Return a list of words
+    """
+    word_list = []
+    o = open(f)
+    for word in o:
+        word_list.append(word.strip('\n\r'))
+    o.close()
+    return word_list
+
+def make_anagrams_dict(l):
+    """
+    1. Create a reference set of characters out of each item from a list of words.
+    2. Search list of words for reference anagrams.
+    3. Return a dictionary with references as keys and a list of their anagrams as values.
+    """
+    anagrams_dict_raw = {}
+    anagrams_dict = {}
+    for word in l:
+        word_to_list = list(word)
+        word_to_list.sort()
+        ref = tuple(word_to_list)
+        if ref not in anagrams_dict_raw:
+            anagrams_dict_raw[ref] = [word]
+        else:
+            anagrams_dict_raw[ref].append(word)
+    for key in anagrams_dict_raw:
+        if len(anagrams_dict_raw[key]) >= 2:
+            anagrams_dict[key] = anagrams_dict_raw[key]
+    return anagrams_dict
+
+if __name__ == '__main__':
+    for ref, words in make_anagrams_dict(list_words(f)).items():
+        print ref, ':', words
