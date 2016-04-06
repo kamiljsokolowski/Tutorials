dbfd2b9244c76d9ed6c33a58b685296f9c319921
Kamil Sokolowski <kamil.j.sokolowski@gmail.com>
2016-04-06 14:37:10 +0200

    fix pillow compilation errors (low memory & additional lib requirements)

--- Tango_with_Django/bootstrap.sh
+++ Tango_with_Django/bootstrap.sh
@@ -2,7 +2,10 @@
 
 # Python
 sudo apt-get update -q && sudo apt-get install -y \
+    python-dev \
     python-setuptools \
+    libjpeg8-dev \
+    zlib1g-dev \
  && sudo easy_install pip \
  && sudo pip install virtualenv \
  && sudo pip install virtualenvwrapper
